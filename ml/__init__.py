578115284982a3c0053a95e1d97cba0d2bee38fc
Greg <ggundersen@gmail.com>
2018-06-19 11:58:32 -0400

    Added library interface imports.

---
+++ ml/__init__.py
@@ -0,0 +1,9 @@
+"""=============================================================================
+Library interface.
+============================================================================="""
+
+from ml import cluster
+from ml import datasets
+from ml import decomposition
+from ml import linalg
+from ml import normalization
