746429eedb26a800fc84957920e1a0493b504014
Gregory Gundersen <ggundersen@gmail.com>
2020-08-17 18:25:35 -0400

    new version of PCCA

--- probabilistic_canonical_correlation_analysis.py
+++ probabilistic_canonical_correlation_analysis.py
@@ -10,220 +10,113 @@ Probabilistic canonical correlation analysis. For references in comments:
 
 import numpy as np
 
-mm     = np.matmul
-diag   = np.diag
-det    = np.linalg.det
-dot    = np.dot
-log    = np.log
-tr     = np.trace
-inv    = np.linalg.inv
-Normal = np.random.multivariate_normal
+inv = np.linalg.inv
 
 
 # -----------------------------------------------------------------------------
 
-class PCCA:
+class ProbabilisticCCA:
 
-    def __init__(self, n_components):
+    def __init__(self, n_components, n_iters):
+        """Initialize probabilistic CCA model.
         """
-        Initialize the probabilistic CCA model.
+        self.k = n_components
+        self.n_iters = n_iters
 
-        :param n_components: The dimensionality of the latent variable.
-        :param rank_k:       The rank to use for low-rank approximation of matrix
-                             inversion.
-        :return:             None.
+    def fit(self, X1, X2):
+        """Fit model via EM.
         """
-        self.n_components = n_components
+        self._init_params(X1, X2)
+        for _ in range(self.n_iters):
+            self._em_step()
 
-    def fit(self, X, n_iters):
+    def transform(self, X1, X2):
+        """Embed data using fitted model.
         """
-        Fit the probabilistic CCA model using Expectation-Maximization.
+        X = np.hstack([X1, X2]).T
+        Psi_inv = inv(self.Psi)
+        M = inv(np.eye(self.k) + self.W.T @ Psi_inv @ self.W)
+        Z = M @ self.W.T @ Psi_inv @ X
+        return Z.T
 
-        :param X:       A joint density, represented as a sequence of datasets,
-                        each with N observations and p1- and p2-dimensions
-                        respectively.
-        :param n_iters: The number of EM iterations.
-        :return:        None.
+    def fit_transform(self, X1, X2):
+        self.fit(X1, X2)
+        return self.transform(X1, X2)
+
+    def sample(self, n_samples):
+        """Sample from the fitted model.
         """
-        assert type(X) is list or type(X) is tuple
-        X1, X2 = X
-        n1, p1 = X1.shape
-        n2, p2 = X2.shape
-        assert n1 == n2
-        n = n1
-        p = p1 + p2
-        k = self.n_components
+        Psi_inv = inv(self.Psi)
+        M = inv(np.eye(self.k) + self.W.T @ Psi_inv @ self.W)
+        Z_post_mean = M @ self.W.T @ Psi_inv @ self.X
 
-        X = np.hstack(X).T
-        assert X.shape == (p, n)
+        X_mean = self.W @ Z_post_mean
+        assert(X_mean.shape == (self.p, n_samples))
+        X = np.zeros((n_samples, self.p))
 
-        nlls = []
+        for i in range(n_samples):
+            X[i] = np.random.multivariate_normal(X_mean[:, i], self.Psi)
 
-        Lambda, Psi = self._init_params(p1, p2)
-        for i in range(n_iters):
-            # print('Iter: %s' % i)
-            Lambda_new, Psi_new = self._em_step(X, Lambda, Psi, n, k)
-            nll = self.neg_log_likelihood(X, Lambda, Psi)
-            nlls.append(nll)
-            Lambda = Lambda_new
-            Psi    = Psi_new
+        return X[:, :self.p1], X[:, self.p1:]
 
-        self.X       = X
-        self.Lambda  = Lambda
-        self.Lambda1 = Lambda[:p1, :]
-        self.Lambda2 = Lambda[p1:, :]
-        self.Psi     = Psi
-        self.Psi1    = Psi[:p1, :p1]
-        self.Psi2    = Psi[p1:, p1:]
-        self.nlls    = nlls
+# -----------------------------------------------------------------------------
 
-    def sample(self, n):
+    def _em_step(self):
+        """Perform EM on parameters W and Psi
         """
-        Sample from the fitted probabilistic CCA model.
+        Psi_inv = inv(self.Psi)
+        M = inv(np.eye(self.k) + self.W.T @ Psi_inv @ self.W)
+        S = M @ self.W.T @ Psi_inv @ self.X
+        A = self.n * M + S @ S.T
 
-        :param n: The number of samples.
-        :return:  Two views of n samples each.
+        W_new = self.X @ S.T @ inv(A)
+
+        W1       = self.W[:self.p1]
+        W1_new   = W_new[:self.p1]
+        Psi1_inv = Psi_inv[:self.p1, :self.p1]
+        Psi1_new = self.Sigma1 - self.Sigma1 @ Psi1_inv @ W1 @ M @ W1_new.T
+
+        W2       = self.W[self.p1:]
+        W2_new   = W_new[self.p1:]
+        Psi2_inv = Psi_inv[self.p1:, self.p1:]
+        Psi2_new = self.Sigma2 - self.Sigma2 @ Psi2_inv @ W2 @ M @ W2_new.T
+
+        Psi_new = np.block([[Psi1_new, np.zeros((self.p1, self.p2))],
+                            [np.zeros((self.p2, self.p1)), Psi2_new]])
+
+        self.W   = W_new
+        self.Psi = Psi_new
+
+    def _init_params(self, X1, X2):
+        """Initialize parameters.
         """
-        Z  = self.E_z_given_x(self.Lambda, self.Psi, self.X)
+        self.X1, self.X2 = X1, X2
+        self.n, self.p1 = self.X1.shape
+        _, self.p2 = self.X2.shape
+        self.p = self.p1 + self.p2
 
-        m1 = dot(self.Lambda1, Z)
-        m2 = dot(self.Lambda2, Z)
+        # Initialize sample covariances matrices.
+        self.X = np.hstack([X1, X2]).T
+        assert(self.X.shape == (self.p, self.n))
+        self.Sigma1 = np.cov(self.X1.T)
+        assert(self.Sigma1.shape == (self.p1, self.p1))
+        self.Sigma2 = np.cov(self.X2.T)
+        assert(self.Sigma2.shape == (self.p2, self.p2))
 
-        p1, _ = self.Lambda1.shape
-        p2, _ = self.Lambda2.shape
+        # Initialize W.
+        W1 = np.random.random((self.p1, self.k))
+        W2 = np.random.random((self.p2, self.k))
+        self.W = np.vstack([W1, W2])
+        assert(self.W.shape == (self.p, self.k))
 
-        X1 = np.zeros((p1, n))
-        X2 = np.zeros((p2, n))
-
-        for i in range(n):
-            X1[:, i] = Normal(mean=m1[:, i], cov=self.Psi1)
-            X2[:, i] = Normal(mean=m2[:, i], cov=self.Psi2)
-
-        return X1.T, X2.T
-
-    def _em_step(self, X, Lambda, Psi, n, k):
-        """
-        Perform Expectation-Maximization on the parameters Lambda and Psi. See
-        my blog post on factor analysis:
-
-        http://gregorygundersen.com/blog/2018/08/08/factor-analysis/
-
-        Or the papers referenced in the docstring for derivations.
-
-        Psi   : (p1 + p2) x (p1 + p2)
-        Lambda: (p1 + p2) x k
-        V     : (p1 + p2) x n
-        """
-        # Update Lambda.
-        # ==============
-        Exp          = self.E_z_given_x(Lambda, Psi, X)
-        Lambda_lterm = dot(X, Exp.T)
-        Lambda_rterm = self.E_zzT_given_x(Lambda, Psi, X, k)
-        Lambda_star  = dot(Lambda_lterm, inv(Lambda_rterm))
-
-        # Update Psi.
-        # ===========
-        Exp      = self.E_z_given_x(Lambda, Psi, X)
-        Psi_new  = dot(X, X.T) - dot(Lambda_star, dot(Exp, X.T))
-        Psi_star = 1./n * np.diag(np.diag(Psi_new))
-
-        return Lambda_star, Psi_star
-
-    def _init_params(self, p1, p2):
-        """
-        :param p1: Dimensionality of the first view of data.
-        :param p2: Dimensionality of the second view of data.
-        :return:
-        """
-        k = self.n_components
-
-        # TODO: How to choose these?
-        Sigma_init = 0.5
-        Sigma1     = Sigma_init
-        Sigma2     = Sigma_init
-
-        self.Lambda1 = np.random.random((p1, k))
-        self.Lambda2 = np.random.random((p2, k))
-
-        Psi1_init = Sigma1 * np.eye(p1)
-        Psi2_init = Sigma2 * np.eye(p2)
-
-        Lambda = np.concatenate((self.Lambda1, self.Lambda2), axis=0)
-        Psi    = np.block([[Psi1_init, np.zeros((p1, p2))],
-                           [np.zeros((p2, p1)), Psi2_init]])
-
-        return Lambda, Psi
-
-    def neg_log_likelihood(self, X, Lambda, Psi):
-        """Compute negative log-likelihood.
-
-        For a derivation of the log-likelihood Q, see Appendix B in:
-
-            The EM Algorithm for Mixtures of Factor Analyzers
-            Ghahramani and Hinton
-            http://mlg.eng.cam.ac.uk/zoubin/papers/tr-96-1.pdf
-        """
-        p, n = X.shape
-        k = self.n_components
-        Q = 0
-
-        Ez  = self.E_z_given_x(Lambda, Psi, X).T
-        Ezz = self.E_zzT_given_x(Lambda, Psi, X, k).T
-        assert Ezz.shape == (k, k)
-        A   = np.diag(mm(mm(X.T, inv(Psi)), X))
-        B   = -2 * np.diag(mm(mm(mm(X.T, inv(Psi)), Lambda), Ez.T))
-        C   = tr(mm(mm(mm(Lambda.T, inv(Psi)), Lambda), Ezz))
-        Q  += (A + B).sum() + C
-
-        D = -n / 2. * log(det(Psi))
-        Q += D
-
-        neg_Q = -Q  # Code clarity: don't miss that negative sign.
-        return neg_Q
-
-    def neg_log_likelihood_archive(self, X, Lambda, Psi):
-        """Compute negative log-likelihood.
-
-        For a derivation of the log-likelihood Q, see Appendix B in:
-
-            The EM Algorithm for Mixtures of Factor Analyzers
-            Ghahramani and Hinton
-            http://mlg.eng.cam.ac.uk/zoubin/papers/tr-96-1.pdf
-        """
-        p, n = X.shape
-        k = self.n_components
-        Q = 0
-
-        for xi in X.T:
-            Ez  = self.E_z_given_x(Lambda, Psi, xi)
-            Ezz = self.E_zzT_given_x(Lambda, Psi, xi, k)
-            A = mm(mm(xi.T, inv(Psi)), xi)
-            B = -2 * (mm(mm(mm(xi.T, inv(Psi)), Lambda), Ez))
-            C = tr(mm(mm(mm(Lambda.T, inv(Psi)), Lambda), Ezz))
-            Q += A + B + C
-
-        D = -n / 2. * log(det(Psi))
-        Q += D
-
-        neg_Q = -Q  # Code clarity: don't miss that negative sign.
-        return neg_Q
-
-    def E_z_given_x(self, L, P, X):
-        beta = mm(L.T, inv(mm(L, L.T) + P))
-        return mm(beta, X)
-
-    def E_zzT_given_x(self, L, P, X, k):
-        beta = mm(L.T, inv(mm(L, L.T) + P))
-        bX   = mm(beta, X)
-        if len(X.shape) == 2:
-            # See here for details:
-            # https://stackoverflow.com/questions/48498662/
-            _, N = X.shape
-            bXXb = np.einsum('ib,ob->io', bX, bX)
-            return N * (np.eye(k) - mm(beta, L)) + bXXb
-        else:
-            bXXb = np.outer(bX, bX.T)
-            return np.eye(k) - mm(beta, L) + bXXb
+        # Initialize Psi.
+        prior_var1 = 1
+        prior_var2 = 1
+        Psi1 = prior_var1 * np.eye(self.p1)
+        Psi2 = prior_var2 * np.eye(self.p2)
+        Psi = np.block([[Psi1, np.zeros((self.p1, self.p2))],
+                        [np.zeros((self.p2, self.p1)), Psi2]])
+        self.Psi = Psi
 
 
 # -----------------------------------------------------------------------------
@@ -232,18 +125,20 @@ class PCCA:
 
 import matplotlib.pyplot as plt
 from   _datasets import load_lowrankcov
+from   sklearn.cross_decomposition import CCA
 
 
 k = 10
-X1, X2 = load_lowrankcov(N=200, P=50, Q=50, k=k)
-pcca = PCCA(n_components=2)
-pcca.fit([X1, X2], n_iters=100)
-X1_, X2_ = pcca.sample(200)
+X1, X2 = load_lowrankcov(N=200, P=50, Q=40, k=k)
 
-fig, ax = plt.subplots()
-ax.scatter(X1[:, 0], X1[:, 1], c='red', marker='.')
-ax.scatter(X2[:, 0], X2[:, 1], c='blue', marker='.')
-ax.scatter(X1_[:, 0], X1_[:, 1], c='orange', marker='*')
-ax.scatter(X2_[:, 0], X2_[:, 1], c='cyan', marker='*')
+pcca = ProbabilisticCCA(n_components=2, n_iters=100)
+Z_pcca = pcca.fit_transform(X1, X2)
 
-plt.savefig('_figures/pcca.png')
+cca = CCA(n_components=2)
+Z_cca1, Z_cca2 = cca.fit_transform(X1, X2)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.imshow(cca.x_loadings_)
+ax2.imshow(pcca.W[:50])
+plt.show()
+# plt.savefig('_figures/pcca.png')
